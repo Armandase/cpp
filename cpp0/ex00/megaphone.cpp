cbdea3591b42dac7b74a07e4d68e9e18e2d30efc
Armandase <adamiens@student.42angouleme.fr>
2023-02-06 18:40:22 +0100

    cpp0 ex00 done

---
+++ cpp0/ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int	main(int argc, char *argv[])
+{
+	if (argc <= 1){
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else{
+		for(size_t i = 1; i < (size_t)argc; i++)
+		{
+			std::string str = argv[i];
+			for (size_t j = 0; j < str.length(); j++)
+				std::cout << (char)std::toupper(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+}
