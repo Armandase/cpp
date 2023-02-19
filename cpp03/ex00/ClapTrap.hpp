14e0950c8809cb628a2162ac4d08bcfe2e7571fc
Armandase <armand.damiens31@gmail.com>
2023-02-19 19:50:47 +0100

    manque main sur cpp03 ex00

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef CLAP_TRAP_H
+# define CLAP_TRAP_H
+
+# include <iostream>
+
+class ClapTrap{
+
+public:
+	ClapTrap(std::string name);
+	~ClapTrap();
+	ClapTrap(const ClapTrap& copy);
+	ClapTrap& operator=(const ClapTrap& clap);
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+private:
+	std::string	_name;
+	int			_hitPoints;
+	int			_energyPoints;
+	int			_attacksDamage;
+};
+
+#endif
