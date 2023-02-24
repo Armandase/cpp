9b7527e295afbb12b23e34e64c97b18ef46eb558
Armandase <adamiens@student.42angouleme.fr>
2023-02-24 15:45:01 +0100

    le 03 il est dure

--- cpp03/ex01/ClapTrap.hpp
+++ cpp03/ex01/ClapTrap.hpp
@@ -10,10 +10,10 @@ public:
 	~ClapTrap();
 	ClapTrap(const ClapTrap& copy);
 	ClapTrap& operator=(const ClapTrap& clap);
-	void	attack(const std::string& target);
+	virtual void	attack(const std::string& target);
 	void	takeDamage(unsigned int amount);
 	void	beRepaired(unsigned int amount);
-protected:
+private:
 	std::string	_name;
 	int			_hitPoints;
 	int			_energyPoints;
