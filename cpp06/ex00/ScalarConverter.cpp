0112f72fa6af2392a13631cfcf363c3ed76d08ed
Armandase <adamiens@student.42angouleme.fr>
2023-03-08 09:05:41 +0100

    tmp pour tester un truc

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
+#include <iomanip>
 
 ScalarConverter::ScalarConverter(){
 	_c = 0;
@@ -25,10 +26,13 @@ ScalarConverter & ScalarConverter::operator=(const ScalarConverter &copy){
 ScalarConverter::ScalarConverter(char *string)
 {
 	convert(string);
-	std::cout << "char :" << _c << std::endl;
-	std::cout << "int :" << _i << std::endl;
-	std::cout << "float :" << _f << std::endl;
-	std::cout << "double :" << _d << std::endl;
+	if (_c == -1)
+		std::cout << "char: " << "Non displayable" << std::endl;
+	else
+		std::cout << "char: " << _c << std::endl;
+	std::cout << "int: " << _i << std::endl;
+	std::cout << std::setprecision(1) << std::fixed << "float: " << _f << "f" << std::endl;
+	std::cout << std::setprecision(1) << std::fixed << "double: " << _d << std::endl;
 }
 
 void	ScalarConverter::convert(char *toConvert){
@@ -64,12 +68,15 @@ void	ScalarConverter::convert(char *toConvert){
 		if (endPtr == toConvert)
 			std::cout << "It's an overflow" << std::endl;
 		else{
-			if (_i > 255 || _i < 0)
+			if (_d > 255 || _d < 0)
 				_c = -1;
 			else
-				_c = static_cast<char>(_i);
-			_f = static_cast<float>(_c);
-			_d = static_cast<double>(_c);
+				_c = static_cast<char>(_d);
+			if (_d > 2147483647 || _d < -2147483648)
+				_i = 0;
+			else
+				_i = static_cast<int>(_d);
+			_f = static_cast<float>(_d);
 		}
 	}
 	if (isFloat(toConvert))
@@ -78,6 +85,17 @@ void	ScalarConverter::convert(char *toConvert){
 		_f = std::strtof(toConvert, &endPtr);
 		if (endPtr == toConvert)
 			std::cout << "It's an overflow" << std::endl;
+		else{
+			if (_f > 255 || _f < 0)
+				_c = -1;
+			else
+				_c = static_cast<char>(_f);
+			if (_f > 2147483647.0 || _f < -2147483648.0)
+				_i = 0;
+			else
+				_i = static_cast<int>(_f);
+			_d = static_cast<double>(_f);
+		}
 	}
 }
 
